284fa9d466e5535d44d689d240927fefcd39a698
antoinebouche <antoinebou44@gmail.com>
2022-08-12 12:27:57 +0200

    ICO smart contract finished & deployed at 0xfbC518d686770f7D09B762d8629625dFE1A12e8F

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x58FFb920a3193B2fEa499FC2a65f454a3485511c";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
